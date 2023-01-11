1ac90fea42d80421908b446b8b85cec6540f9bd8
GEUNHOKIM <rmszkf@naver.com>
2023-01-12 00:06:47 +0900

    feat.user_playlist.page

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
+import requests
+from bs4 import BeautifulSoup
+
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://test:sparta@cluster0.8nebtyu.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.dbsparta
+
 @app.route('/')
 def home():
    return render_template('index.html')
+
+@app.route("/all_user", methods=["GET"])
+def playlist_get():
+    all_playlist = list(db.test.find({}, {'_id': False}))
+    return jsonify({'playlists':all_playlist})
+
+@app.route("/user", methods=["GET"])
+def user_id():
+    id_receive = 'id_give'
+    # other_receive = 'id_give'
+    return render_template(my_id=id_receive)
+
+
+
 
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
